eb1856220b496b5a84b4f0b491d9cef9e1369912
yuvrajj18 <159643167+yuvrajj18@users.noreply.github.com>
2025-01-05 01:39:35 +0530

    Add files via upload

---
+++ 11_break&continue_.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// for (int i = 0; i < 40; i++)
+// {
+//     cout<<i<<endl;
+//     if(i==20){
+//     break;
+//     }
+// }
+
+
+for (int i = 0; i < 40; i++)
+{
+   
+    if(i==20){
+    continue;
+    }
+    cout<<i<<endl;
+}
+     return (0);
+
+}
