7f46f408b8afed1b467e0f682bf2757d0ac4f4d5
yuvrajj18 <159643167+yuvrajj18@users.noreply.github.com>
2025-01-05 01:15:43 +0530

    Add files via upload

---
+++ 10_for_while_do-while.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// for (int i = 0; i < 40; i++)
+// {
+//     cout<<i<<endl;
+// }
+
+
+// int i = 1;
+// while(i<=100){
+//     cout<<i<<endl;
+//     i++;
+// }
+
+
+// i = 1;
+// while(true){
+//     cout<<i<<endl;
+//     i++;
+// }
+
+
+
+int a = 6;
+int i = 1;
+do{
+    cout<<a*i<<endl;
+    i++;
+
+}while(i<=10);
+    return 0;
+}
